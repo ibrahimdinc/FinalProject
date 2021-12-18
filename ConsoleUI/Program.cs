3b86185a19cdd08a863c4885340fe738f4add9e1
ibrahim dinç <iboi336633@gmail.com>
2021-12-18 21:07:27 +0300

    18 Aralık

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,12 +9,36 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+            //CategoryTest();
+            ProductTest2();
+
+        }
+
+        private static void ProductTest2()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "++" + product.CategoryName);
+            }
+        }
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
             foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
             }
-            
         }
     }
 }
